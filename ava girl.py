3a370f1720630a47a9e6f8e4a2eae10d4c34bf84
ashwinkck <151431555+ashwinkck@users.noreply.github.com>
2024-09-22 10:29:33 +0530

    Update ava girl.py

--- ava girl.py
+++ ava girl.py
@@ -5,7 +5,7 @@ import pyttsx3
 import time
 
 # Replace with your actual Gemini API key
-your_api_key = "AIzaSyB05rK4s92VBjOKRbmvqXFy9Y4QT3bQUlg"  # Replace with your actual key
+your_api_key = "YOUR GEMINI API HERE"  # Replace with your actual key
 
 genai.configure(api_key=your_api_key)
 
